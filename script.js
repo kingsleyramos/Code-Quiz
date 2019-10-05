8daa3aba196a327167191d2c47ab1d1cf85f2474
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-10-05 10:01:37 -0700

    Created files

---
+++ script.js
@@ -0,0 +1,133 @@
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>","<scripting>","<javascript>","<js>"],
+        answer: "<script>"
+    },
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["The <head> section","Both the <head> section and the <body> section are correct","The <body> section","Neither"],
+        answer: "The <body> section"
+    },
+    {
+        title: "WThe external JavaScript file must contain the <script> tag.",
+        choices: ["True","False"],
+        answer: "False"
+    },
+    {
+        title: "How do you write \"Hello World\" in an alert box?",
+        choices: ["msg(\"Hello World\");","alert(\"Hello World\");","msgBox(\"Hello World\");","alertBox(\"Hello World\");"],
+        answer: "alert(\"Hello World\");"
+    },
+    {
+        title: "How do you create a function in Javascript?",
+        choices: ["function myFunction()","function:myFuction","fuction = myFunction()"],
+        answer: "function myFunction()"
+    },
+    {
+        title: "How do you call a function named \"myFunction\"?",
+        choices: ["call myFunction","call function myFunction()","myFunction()"],
+        answer: "myFunction()"
+    },
+    {
+        title: "How do you write an IF statment in Javascript?",
+        choices: ["if i == 5 then","if i = 5 then","if (i == 5)","if i = 5"],
+        answer: "if (i == 5)"
+    },
+    {
+        title: "How to write an IF statement for executing some code if \"i\" is NOT equal to 5?",
+        choices: ["if i <> 5","if (i <> 5)","if i=! 5 then","if (i != 5)"],
+        answer: "if (i != 5)"
+    },
+    {
+        title: "How does a WHILE loop start?",
+        choices: ["while i = 1 to 10","while (i <= 10)","while (i <= 10; i++)","while (i)"],
+        answer: "while (i <= 10)"
+    },
+    {
+        title: "How does a FOR loop start?",
+        choices: ["for i = 1 to 5","for (i <= 5; i++)","for (i = 0; i <= 5; i++)","for (i = 0; i <= 5)"],
+        answer: "for (i = 0; i <= 5; i++)"
+    },
+    {
+        title: "How can you add a comment in a JavaScript",
+        choices: ["'This is a comment","//this is a comment","<!--This is a comment-->"],
+        answer: "//this is a comment"
+    },
+    {
+        title: "What is the correct way to write a JavaScript array?",
+        choices: ["var colors = \"red\", \"green\", \"blue\"","var colors = [\"red\", \"green\", \"blue\"]","var colors = (1:\"red\", 2:\"green\", 3:\"blue\")","var colors = 1 = (\"red\"), 2 = (\"green\"), 3 = (\"blue\")"],
+        answer: "var colors = [\"red\", \"green\", \"blue\"]"
+    },
+    {
+        title: "What does HTML stand for?",
+        choices: ["Home Tool Markup Language","Hyper Text Markup Language","Hyperlinks and Text Markup Language"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        title: "Choose the correct HTML element for the largest heading:",
+        choices: ["<h6>","<head>","<h1>","<heading>"],
+        answer: "<h1>"
+    },
+    {
+        title: "What is the correct HTML element for inserting a line break?",
+        choices: ["<lb>","<break>","<br>"],
+        answer: "<br>"
+    },
+    {
+        title: "What is the correct HTML for adding a background color?",
+        choices: ["<body bg=\"yellow\">","<body style=\"background-color:yellow;\">"," <background>yellow</background>"],
+        answer: "<body style=\"background-color:yellow;\">"
+    },
+    {
+        title: "Choose the correct HTML element to define important text",
+        choices: ["<b>","<i>","<important>","<strong>"],
+        answer: ""
+    },
+    {
+        title: "Choose the correct HTML element to define emphasized text",
+        choices: ["<i>","<em>","<italic>"],
+        answer: "<i>"
+    },
+    {
+        title: "Which character is used to indicate an end tag?",
+        choices: ["/","<","*","^"],
+        answer: "/"
+    },
+    {
+        title: "How can you make a numbered list?",
+        choices: ["<ol>","<list>","<ul>","<dl>"],
+        answer: "<ol>"
+    },
+    {
+        title: "What is the correct HTML for inserting an image?",
+        choices: ["<img href=\"image.gif\" alt=\"MyImage\">"," <image src=\"image.gif\" alt=\"MyImage\">","<img alt=\"MyImage\">image.gif</img>"," <img src=\"image.gif\" alt=\"MyImage\">"],
+        answer: "<image src=\"image.gif\" alt=\"MyImage\">"
+    },
+    {
+        title: "Which HTML element defines the title of a document?",
+        choices: ["<meta>","<head>","<title>","<header>"],
+        answer: "<title>"
+    },
+    {
+        title: "What is the correct HTML for making a text area?",
+        choices: ["<input type=\"textarea\">","<input type=\"textbox\">","<textarea>"],
+        answer: "<textarea>"
+    },
+    {
+        title: "An <iframe> is used to display a web page within a web page.",
+        choices: ["True","False","There is no such thing as an <iframe>"],
+        answer: "True"
+    },
+    {
+        title: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+        choices: ["alt","title","src","longdesc"],
+        answer: "alt"
+    },
+    {
+        title: "Which HTML element is used to specify a header for a document or section?",
+        choices: ["<header>","<section>","<top>","<head>"],
+        answer: ""
+    },
+  ];
+  
